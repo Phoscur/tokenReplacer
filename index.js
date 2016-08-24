3544697b03bf67c9f55680053b9129791bf3a921
Phoscur <phoscur@pheelgood.net>
2016-08-24 21:29:53 +0200

    add scope parser object, go interactive with livereload

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-'use strict'
+'use strict';
+/* globals angular */
 
 angular
   .module('tokenResolver', [])
   .controller('ParserCtrl', function($scope, parseText) {
-    $scope.input = 'This is a {test} text \\{test}';
-    $scope.output = parseText($scope.input, {test: 'tasty'}) || '---';
+    var parser = $scope.parser = {
+      input: '',
+      replacements: {},
+      output: '',
+      replaces: function(token, replacement, oldToken) {
+        if (oldToken || oldToken === '') {
+          delete this.replacements[oldToken];
+        }
+        this.replacements[token] = replacement;
+        return this;
+      },
+      parse: function() {
+        this.output = parseText(this.input, this.replacements) || '---';
+        return this;
+      }
+    };
+
+    // init
+    parser.input = 'This is a {test} text containing \\{test} tokens, \n linebreaks \n \n \\backslashes\\\\\\\\ and {blubb} maybe {recursive} tokens!';
+    parser.replaces('test', 'tasty');
+    parser.replaces('blubb', 'more replaced tokens');
+    parser.replaces('recursive', '{test}');
+    parser.parse();
   })
   .factory('parseText', function() {
     return function(text, replacements) {
@@ -13,8 +35,15 @@ angular
           /([^\\])\{([a-zA-Z_][a-zA-Z0-9_]*)\}/g,
           function(match, previousChar, token, offset, string) {
             // previous char is matched not to be a backslash
-            return previousChar + (replacements[token] || token);
+            var replaced = previousChar;
+            if (!replacements[token]) {
+              // no replacement found, reconstruct previous text
+              replaced += '{' + token + '}';
+            } else {
+              replaced += replacements[token];
+            }
+            return replaced;
           }
         );
     };
-  })
+  });
