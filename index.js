fb67f6b6dbfd02736403db7884aa39d777fa3a83
Phoscur <phoscur@pheelgood.net>
2016-08-24 20:32:30 +0200

    parse text!

--- index.js
+++ index.js
@@ -2,7 +2,19 @@
 
 angular
   .module('tokenResolver', [])
-  .controller('ParserCtrl', function($scope) {
-    $scope.input = 'This is a {test} text';
-    $scope.output = '---';
+  .controller('ParserCtrl', function($scope, parseText) {
+    $scope.input = 'This is a {test} text \\{test}';
+    $scope.output = parseText($scope.input, {test: 'tasty'}) || '---';
+  })
+  .factory('parseText', function() {
+    return function(text, replacements) {
+      return text
+        .replace(
+          /([^\\])\{([a-zA-Z_][a-zA-Z0-9_]*)\}/g,
+          function(match, previousChar, token, offset, string) {
+            // previous char is matched not to be a backslash
+            return previousChar + (replacements[token] || token);
+          }
+        );
+    };
   })
